4ffa20adc3fc25e790c444f8f0899943796be6e3
ChaoJie Yang <57297482+CJ-Yang0225@users.noreply.github.com>
2021-05-30 00:08:50 +0800

    chore: add loaders and deps for styles and assets

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -3,16 +3,83 @@ const { resolve } = require('path');
 const path = require('path');
 const { PROJECT_PATH, isDev } = require('../constants');
 
+const getCssLoaders = (importLoaders) => [
+  'style-loader',
+  {
+    loader: 'css-loader',
+    options: {
+      modules: false,
+      sourceMap: isDev,
+      importLoaders,
+    },
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
+        ident: 'postcss',
+        plugins: [
+          // 修復一些和 flex 佈局相關的 bug
+          require('postcss-flexbugs-fixes'),
+          require('postcss-preset-env')({
+            autoprefixer: {
+              grid: true,
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+          }),
+          require('postcss-normalize'),
+        ],
+      },
+      sourceMap: isDev,
+    },
+  },
+];
+
 module.exports = {
   entry: {
     app: path.resolve(PROJECT_PATH, './src/index.ts'),
   },
   output: {
     filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
+    assetModuleFilename: 'images/[name].[contenthash:8].[ext]',
     path: path.resolve(PROJECT_PATH, './dist'),
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [...getCssLoaders(1)],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          ...getCssLoaders(2),
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2?)$/,
+        type: 'asset/resource',
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
